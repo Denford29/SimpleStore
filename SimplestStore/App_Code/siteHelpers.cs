0e03f1b36f2c4a0e9498e5d3e802601ff0a3ac0b
denfordmutseriwa <denfordmutseriwa@yahoo.com>
2019-11-25 23:36:19 +1030

    Initial Commit

---
+++ SimplestStore/App_Code/siteHelpers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using umbraco;
+
+namespace SimplestStore.App_Code
+    {
+    public class siteHelpers
+        {
+        public static string LimitWordsInText(string stringText, int MaxWords)
+            {
+            var limitedText = "";
+            if (!string.IsNullOrEmpty(stringText))
+                {
+                var nonHtmlString = library.StripHtml(stringText);
+                var wordsListArray = nonHtmlString.Replace("\"", "").Split(' ');
+                var wordCounter = 0;
+
+                foreach (var word in wordsListArray)
+                    {
+                    wordCounter++;
+                    if (wordCounter <= MaxWords)
+                        {
+                        limitedText += word.Trim() + " ";
+                        }
+                    }
+                }
+            return limitedText;
+            }
+
+        }
+    }
---
+++ SimplestStore/Scripts/siteJS.js
@@ -0,0 +1,50 @@
+﻿$().ready(function () {
+
+    $("#validationError").hide();
+    $("#submitError").hide();
+
+    $('.cc-number').payment('formatCardNumber');
+    $('.cc-exp').payment('formatCardExpiry');
+    $('.cc-cvc').payment('formatCardCVC');
+
+    $('#cardDetailsForm').submit(function (event) {
+
+        var $form = $(this);
+
+        $("#cardDetailsForm").validate({
+            CardholdersName: {
+                required: true
+            },
+
+            CardNumber: {
+                required: true
+            },
+
+            CardExpiryDate: {
+                required: true
+            },
+
+            CardCCV: {
+                required: true
+            },
+            errorPlacement: function (error, element) {
+            },
+            invalidHandler: function (event, validator) {
+                var errors = validator.numberOfInvalids();
+                if (errors) {
+                    var message = "<h3>Form error!</h3><p>We're sorry, there was a problem submitting your form. Please check the fields highlighted below to resolve any errors.</p>";
+                    $("#validationError").html(message);
+                    $("#validationError").show();
+                }
+                else {
+                    $("#validationError").hide();
+                }
+            },
+            submitHandler: function () {
+                $(window).spin();
+            }
+        });
+        return false;
+    });
+
+});
